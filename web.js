302a8dc78227f23873edbc1bc736bf5bd0d7bd72
MDzxbotz <126398155+MDzxbotz@users.noreply.github.com>
2025-12-23 22:39:27 +0530

    new testing

---
+++ web.js
@@ -0,0 +1,13 @@
+// let name = prompt("enter your name: ");
+
+console.log("running js");
+
+alert("dumnmy warning")
+
+const doc = document.getElementsByClassName("count")
+
+console.log(doc)
+
+// const createPage = (req, res) = {
+
+// }
