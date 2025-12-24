7a49a2ce7e48157ab337c85e682a2efb0f48246a
MDzxbotz <126398155+MDzxbotz@users.noreply.github.com>
2025-12-24 13:21:35 +0530

    new changes

--- web.js
+++ web.js
@@ -11,3 +11,35 @@ console.log(doc)
 // const createPage = (req, res) = {
 
 // }
+
+let tutorials = [
+    {
+     url: "dark.jpg",
+     description: "send command /special_link<br>click the create button dummy js",
+     count: 1
+    },
+    {
+    url: "small-tut-2.jpg",
+    description: "after send the message you want to store to bot dium,mmyt description more to text uint",
+    count: 2
+    }
+];
+
+function createPage(){
+    
+    const doc = document.getElementById("tutoriald");
+    // for (const fruit of fruits){...}
+    for(let i = 0; i< tutorials.length; i++){
+        const div = document.createElement("div")
+        div.className = "tutorials";
+        div.innerHTML = `
+             <img class="tutorial-img" src="${tutorials[i]['url']}" alt="tutorial 1 image">
+             <p class="count">${tutorials[i]["count"]}</p>
+             <p class="description">${tutorials[i]['description']}</p>
+        `
+        doc.appendChild(div)
+        
+    }
+}
+
+createPage()
